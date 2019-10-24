c00ce333511f9ee05a4f8e8f17efdae1dc289e88
thaliafreitas <freitas.thalia97@gmail.com>
2019-10-24 18:07:00 -0300

    Initial Commit

---
+++ WhoIsAliveFromRickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoIsAliveFromRickAndMorty
+//
+//  Created by Thalia Freitas on 24/10/19.
+//  Copyright © 2019 Thalia Freitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhoIsAliveFromRickAndMortyTests/WhoIsAliveFromRickAndMortyTests.swift
@@ -0,0 +1,34 @@
+//
+//  WhoIsAliveFromRickAndMortyTests.swift
+//  WhoIsAliveFromRickAndMortyTests
+//
+//  Created by Thalia Freitas on 24/10/19.
+//  Copyright © 2019 Thalia Freitas. All rights reserved.
+//
+
+import XCTest
+@testable import WhoIsAliveFromRickAndMorty
+
+class WhoIsAliveFromRickAndMortyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
