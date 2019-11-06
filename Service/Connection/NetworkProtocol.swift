9d9891881118fd414d13fd811dbce24229314d2d
thaliafreitas <freitas.thalia97@gmail.com>
2019-11-06 15:00:03 -0300

    add queryItem to NetworkProtocol to search items whit api queries

--- Service/Connection/NetworkProtocol.swift
+++ Service/Connection/NetworkProtocol.swift
@@ -10,6 +10,6 @@ import Foundation
 
 protocol NetworkProtocol {
     associatedtype EndPoint: EndPointType
-    func perform<T: Decodable>(_ route: EndPoint, completion: @escaping (T?, Errors?) -> (Void))
+    func perform<T: Decodable>(_ route: EndPoint, queryItem: [String: Any]?, completion: @escaping (T?, Errors?) -> (Void))
     func cancel()
 }
